9342e1db71b30f4c43ed3ad1812956b7f89e7435
weebpal-lamctm <caolam1710@gmail.com>
2019-03-06 19:46:13 +0700

    Future-test thay doi

--- Form1.cs
+++ Form1.cs
@@ -23,6 +23,7 @@ namespace Test_Winform
         private void button1_Click(object sender, EventArgs e)
         {
             int a = int.Parse(label1.Text);
+            int b = int.Parse(label1.Text);
         }
     }
 }
